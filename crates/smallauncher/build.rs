aabb074d2572c88362aa196703e12b9e241b5176
Heráclito <heraclitoqsaldanha@gmail.com>
2025-02-11 07:58:38 -0300

    fix cargo install on windows

--- crates/smallauncher/build.rs
+++ crates/smallauncher/build.rs
@@ -1,7 +1,9 @@
 fn main() {
 	if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
 		let mut res = winresource::WindowsResource::new();
-		res.set_icon("../../assets/icon.ico");
-		res.compile().unwrap();
+		res.set_icon("assets/icon.ico");
+		if let Err(e) = res.compile() {
+			println!("cargo::warning={e}");
+		}
 	}
 }
